bd7a3a5f90369053a9d40c51142f718ff974050f
mmshuwaki <mmshuwaki@gmail.com>
2025-03-09 15:42:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "Youokay801@gmail.com";
-const pass = "cnwupnrjfguifecl";
+//const pass = "cnwupnrjfguifecl";
 // 01 APRIL
 
 // Middleware
